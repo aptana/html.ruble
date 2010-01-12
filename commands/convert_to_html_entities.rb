5427d044f5680e2d18f40741c1782164822a8c84
sgtcoolguy <cwilliams@aptana.com>
2010-01-12 15:25:50 -0500

    convert the Convert to HTML Entities command

---
+++ commands/convert_to_html_entities.rb
@@ -0,0 +1,12 @@
+require 'radrails'
+    
+command 'Convert Character / Selection to Entities' do |cmd|
+  cmd.key_binding = 'M1+M2+7'
+  cmd.scope = 'text.html'
+  cmd.output = :replace_selection
+  cmd.input = :selection, :character
+  cmd.invoke do |context|
+    $KCODE = 'U'
+    encode(context.in.read)
+  end
+end
