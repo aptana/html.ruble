65bc0bc4b22472fb03df1d8ef8f954c6c2b54e6d
unknown <Christopher Williams@.(none)>
2010-02-26 09:44:07 -0800

    fix to escape filepath for URI form properly (to work on windows)

--- commands/preview_in_external_browser.rb
+++ commands/preview_in_external_browser.rb
@@ -1,4 +1,5 @@
 require 'ruble'
+require 'uri'
 
 command 'Open Document in Default Browser' do |cmd|
   cmd.key_binding = "M1+M2+P"
@@ -9,7 +10,7 @@ command 'Open Document in Default Browser' do |cmd|
     if ENV['TM_PROJECT_SITEURL']
       url = "#{ENV['TM_PROJECT_SITEURL']}" + ENV['TM_FILEPATH'].sub(/^#{Regexp.escape(ENV['TM_PROJECT_DIRECTORY'])}\//, '') 
     else
-      url = "file://#{ENV['TM_FILEPATH']}"
+      url = "file:#{URI.escape(ENV['TM_FILEPATH'])}"
     end    
     context.browser.open(url, :browser => :default)
   end
