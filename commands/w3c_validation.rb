11a6c5526e9143e8a858daf1cea0da0472ef1ed9
sgtcoolguy <chris.a.williams@gmail.com>
2010-07-28 11:19:18 -0400

    Just use net/http, no need for HTTPClient library

--- commands/w3c_validation.rb
+++ commands/w3c_validation.rb
@@ -9,17 +9,21 @@ command 'Validate Syntax (W3C)' do |cmd|
     $KCODE = 'U'
     page = $stdin.read
     page.gsub!(/<\?(php|=).*?\?>|<%.*?%>/m, '')
-
-    require 'httpclient'
-    # try direct input path
-    result = HTTPClient.post 'http://validator.w3.org/check', { :fragment => page }
     
-    content = result.content
+    w3c_url = 'http://validator.w3.org/check'
+    
+    require 'net/http'
+    require 'uri'
+    
+    response = Net::HTTP.post_form(URI.parse(w3c_url), {'ss' => "1", 'fragment' => page})
+    status = response['x-w3c-validator-status']
+    
+    content = response.body
     content = content.gsub(/<\/title>/, '\&<base href="http://validator.w3.org/">')
-    content.gsub!(/Line (\d+),? Column (\d+)/i) do
-      # FIXME These links won't work for us!
-      "<a href='txmt://open?line=#$1&column=#{$2.to_i + 1}'>#$&</a>"
-    end
+    # content.gsub!(/Line (\d+),? Column (\d+)/i) do
+    #   # FIXME These links won't work for us!
+    #   "<a href='txmt://open?line=#\$1&column=#{\$2.to_i + 1}'>#\$&</a>"
+    # end
     content
   end
 end
