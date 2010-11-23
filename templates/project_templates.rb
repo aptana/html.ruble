3d2e9fa8ff94f088fe11553f903d667cd7cdd696
Kevin Lindsey <klindsey@aptana.com>
2010-11-23 10:15:08 -0600

    Create project template elements for the project template properties that were removed

---
+++ templates/project_templates.rb
@@ -0,0 +1,19 @@
+require 'ruble'
+
+project_template "Basic Web Template" do |t|
+  t.type = :web
+  t.location = "templates/basic_web_template.zip"
+  t.description = "A basic template which includes only a default index.html file"
+end
+
+project_template "HTML5 Boilerplate" do |t|
+  t.type = :web
+  t.location = "git://github.com/paulirish/html5-boilerplate.git"
+  t.description = "Paul Irish's HTML5 Boilerplate (http://html5boilerplate.com/). Requires network access."
+end
+
+project_template "HTML5 Boilerplate (Cached)" do |t|
+  t.type = :web
+  t.location = "templates/html5_template.zip"
+  t.description = "Cached version of Paul Irish's HTML5 Boilerplate project for those without network access, current as of 2010-08-18."
+end
