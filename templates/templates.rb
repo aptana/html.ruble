093f03502d448f3e48beedaf724c2cb21997da0b
Allen Yeung <ayeung@aptana.com>
2011-07-20 14:19:02 -0700

    Fix APSTUD-2134:Add .location property to file templates

--- templates/templates.rb
+++ templates/templates.rb
@@ -15,48 +15,27 @@ HTML_TEMPLATES.each do |filename, name|
   filetypes.each do |filetype|
     template(name + " Template (#{filetype})") do |t|
       t.filetype = filetype
-      t.invoke do |context|
-        ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-        raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/#{filename}")
-        raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
-      end
+      t.location = "templates/#{filename}"
     end
   end
 end
 
 template("HTML 5 Template") do |t|
   t.filetype = "*.html"
-  t.invoke do |context|
-    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-    ENV['TM_YEAR'] = Time.now.strftime("%Y")
-    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/html5.html")
-    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
-  end
+  t.location = "templates/html5.html"
 end
 
 template("IDL Template") do |t|
   t.filetype = "*.idl"
-  t.invoke do |context|
-    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.idl")
-    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
-  end
+  t.location = "templates/template.idl"
 end
 
 template("DTD Template") do |t|
   t.filetype = "*.dtd"
-  t.invoke do |context|
-    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.dtd")
-    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
-  end
+  t.location = "templates/template.dtd"
 end
 
 template("SVG Template") do |t|
   t.filetype = "*.svg"
-  t.invoke do |context|
-    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
-    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.svg")
-    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
-  end
+  t.location = "templates/template.svg"
 end
