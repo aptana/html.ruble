edc4212f57e4d05d834cd955e6ed53a910e28fea
Allen Yeung <ayeung@aptana.com>
2011-07-21 15:21:25 -0700

    revert template change

--- templates/templates.rb
+++ templates/templates.rb
@@ -15,27 +15,48 @@ HTML_TEMPLATES.each do |filename, name|
   filetypes.each do |filetype|
     template(name + " Template (#{filetype})") do |t|
       t.filetype = filetype
-      t.location = "templates/#{filename}"
+      t.invoke do |context|
+        ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+        raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/#{filename}")
+        raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+      end
     end
   end
 end
 
 template("HTML 5 Template") do |t|
   t.filetype = "*.html"
-  t.location = "templates/html5.html"
+  t.invoke do |context|
+    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+    ENV['TM_YEAR'] = Time.now.strftime("%Y")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/html5.html")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+  end
 end
 
 template("IDL Template") do |t|
   t.filetype = "*.idl"
-  t.location = "templates/template.idl"
+  t.invoke do |context|
+    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.idl")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+  end
 end
 
 template("DTD Template") do |t|
   t.filetype = "*.dtd"
-  t.location = "templates/template.dtd"
+  t.invoke do |context|
+    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.dtd")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+  end
 end
 
 template("SVG Template") do |t|
   t.filetype = "*.svg"
-  t.location = "templates/template.svg"
+  t.invoke do |context|
+    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.svg")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+  end
 end
