a8d80451e03cd1f92f8653729cc4724ff4bca881
Nam Le <nle@appcelerator.com>
2011-11-07 14:19:50 -0800

    Update project template for HTML ruble to support id field

--- templates/project_templates.rb
+++ templates/project_templates.rb
@@ -2,6 +2,7 @@ require 'ruble'
 
 project_template "Basic Web Template" do |t|
   t.type = :web
+  t.id = "com.aptana.project.template.web.basic"
   t.location = "templates/basic_web_template.zip"
   t.description = "A basic template which includes only a default index.html file"
   t.replace_parameters = false
@@ -9,6 +10,7 @@ end
 
 project_template "HTML5 Boilerplate" do |t|
   t.type = :web
+  t.id = "com.aptana.project.template.web.html5"
   t.location = "git://github.com/h5bp/html5-boilerplate.git"
   t.description = "Paul Irish's HTML5 Boilerplate (http://html5boilerplate.com/). Requires network access."
   t.replace_parameters = false
@@ -16,6 +18,7 @@ end
 
 project_template "HTML5 Boilerplate (Cached)" do |t|
   t.type = :web
+  t.id = "com.aptana.project.template.web.html5.cached"
   t.location = "templates/html5_template.zip"
   t.description = "Cached version of Paul Irish's HTML5 Boilerplate project for those without network access, current as of 2011-08-01."
   t.replace_parameters = false
