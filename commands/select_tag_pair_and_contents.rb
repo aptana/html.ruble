ef3867a102219b3ba2ba2b596dfe546b93328caf
Chris Williams <cwilliams@appcelerator.com>
2012-06-04 12:44:24 -0400

    Fix APSTUD-2129 "select tag area" feature

--- commands/select_tag_pair_and_contents.rb
+++ commands/select_tag_pair_and_contents.rb
@@ -7,7 +7,7 @@ command t(:select_tag_pair_and_content) do |cmd|
   cmd.input = :none
   cmd.invoke do |context|
     offset = context.editor.caret_offset
-    root_node = context.editor.editor_part.file_service.parse_result
+    root_node = context.editor.editor_part.getAST
     node = root_node.getNodeAtOffset(offset)
     # This selects the open tag, the content and the end tag
     context.editor.selection = (node.getStartingOffset...node.getEndingOffset + 1)
