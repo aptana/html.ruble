ed7bfb1cfd97992db410701a790008307ac2006e
Christopher Williams <cwilliams@appcelerator.com>
2012-09-21 09:55:05 -0400

    Add logo for HTML 5 template, add "Web" tag for all

--- templates/project_templates.rb
+++ templates/project_templates.rb
@@ -2,6 +2,7 @@ require 'ruble'
 
 project_template t(:basic_web_template_name) do |t|
   t.type = :web
+  t.tags = ['Web']
   t.id = "com.aptana.project.template.web.basic"
   t.location = "templates/basic_web_template.zip"
   t.description = t(:basic_web_template_desc)
@@ -11,6 +12,8 @@ end
 
 project_template t(:html5_template_name) do |t|
   t.type = :web
+  t.tags = ['Web']
+  t.icon = "templates/HTML5_Logo_64.png"
   t.id = "com.aptana.project.template.web.html5"
   t.location = "git://github.com/h5bp/html5-boilerplate.git"
   t.description = t(:html5_template_desc)
@@ -20,6 +23,8 @@ end
 
 project_template t(:cached_html5_template_name) do |t|
   t.type = :web
+  t.tags = ['Web']
+  t.icon = "templates/HTML5_Logo_64.png"
   t.id = "com.aptana.project.template.web.html5.cached"
   t.location = "templates/html5_template.zip"
   t.description = t(:cached_html5_template_desc)
